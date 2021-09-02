015248307a0ed79db1f404702d5d5b3e0d9b6ef6
wtser <wangtieshou@gmail.com>
2021-09-02 18:08:02 +0800

    Format js code

--- example/admin/config.go
+++ example/admin/config.go
@@ -38,7 +38,7 @@ func NewConfig() (b *presets.Builder) {
 
 	b = presets.New()
 	js, _ := assets.ReadFile("assets/fontcolor.min.js")
-	richeditor.Plugins = []string{"alignment", "video", "fontcolor"}
+	richeditor.Plugins = []string{"alignment", "video", "fontcolor", "imageinsert"}
 	richeditor.PluginsJS = [][]byte{js}
 	b.ExtraAsset("/redactor.js", "text/javascript", richeditor.JSComponentsPack())
 	b.ExtraAsset("/redactor.css", "text/css", richeditor.CSSComponentsPack())
