c79f1d6b59d7d5ba62a2991651abc61db5644894
Charles Shen <shenzhaoyan@gmail.com>
2021-10-27 11:14:27 +0800

    Add version to Page

--- pagebuilder/models.go
+++ pagebuilder/models.go
@@ -1,23 +1,42 @@
 package pagebuilder
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/qor/qor5/publish"
 )
 
 type Page struct {
-	ID    uint
+	ID    uint `gorm:"primarykey"`
 	Title string
 	Slug  string
 	Ti    *time.Time
 
 	publish.Status
 	publish.Schedule
+	publish.Version
 }
 
 func (*Page) TableName() string {
 	return "page_builder_pages"
+}
+
+func (p *Page) PrimarySlug() string {
+	return fmt.Sprintf("%v_%v", p.ID, p.Version.Version)
+}
+
+func (p *Page) PrimaryColumnValuesBySlug(slug string) [][]string {
+	segs := strings.Split(slug, "_")
+	if len(segs) != 2 {
+		panic("wrong slug")
+	}
+
+	return [][]string{
+		{"id", segs[0]},
+		{"version", segs[1]},
+	}
 }
 
 type Container struct {
--- publish/version.go
+++ publish/version.go
@@ -4,7 +4,7 @@ type Version struct {
 	Version       string `gorm:"primary_key;size:128"`
 	VersionName   string
 	ParentVersion string
-	OnlineVersion string `gorm:"index"`
+	OnlineVersion bool `gorm:"index;default:false"`
 }
 
 func (version Version) GetVersionName() string {
