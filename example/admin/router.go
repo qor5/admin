7d84252e6752b9cf596d72f0065d7882ad866690
chenxin0723 <870720136@qq.com>
2021-11-22 17:40:31 +0900

    add seo example

--- example/admin/router.go
+++ example/admin/router.go
@@ -1,9 +1,12 @@
 package admin
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/goplaid/web"
+	"github.com/qor/qor5/example/models"
+	"github.com/qor/qor5/sitemap"
 )
 
 func Router() (mux *http.ServeMux) {
@@ -21,7 +24,22 @@ div {
 `)))
 
 	mux.Handle("/admin/page_builder/", authenticate(c.lb)(c.pageBuilder))
+	// example of seo
+	mux.Handle("/posts/first", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var post models.Post
+		db.First(&post)
+		seodata, _ := SeoCollection.Render(post, r).MarshalHTML(r.Context())
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprintf(w, `<html><head>%s</head><body>%s</body></html>`, seodata, post.Body)
+	}))
 	mux.Handle("/", authenticate(c.lb)(c.pb))
+
+	// example of sitemap and robot
+	sitemap.SiteMap("product").RegisterRawString("https://dev.qor5.com/admin", "/product").MountTo(mux)
+	robot := sitemap.Robots()
+	robot.Agent(sitemap.AlexaAgent).Allow("/product1", "/product2").Disallow("/admin")
+	robot.Agent(sitemap.GoogleAgent).Disallow("/admin")
+	robot.MountTo(mux)
 
 	return
 }
--- example/main.go
+++ example/main.go
@@ -7,16 +7,10 @@ import (
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/qor/qor5/example/admin"
-	"github.com/qor/qor5/sitemap"
 )
 
 func main() {
 	mux := admin.Router()
-	sitemap.SiteMap("product").RegisterRawString("https://dev.qor5.com/admin", "/product").MountTo(mux)
-	robot := sitemap.Robots()
-	robot.Agent(sitemap.AlexaAgent).Allow("/product1", "/product2").Disallow("/admin")
-	robot.Agent(sitemap.GoogleAgent).Disallow("/admin")
-	robot.MountTo(mux)
 
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
