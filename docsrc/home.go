dca6705ca10e9f29dc4a38c10b4145841813d083
Raven <enn.raven@gmail.com>
2021-11-30 16:43:00 +0800

    Basic document structure

--- docsrc/home.go
+++ docsrc/home.go
@@ -2,25 +2,32 @@ package docsrc
 
 import (
 	"embed"
+	"github.com/theplant/htmlgo"
 
 	. "github.com/theplant/docgo"
 )
 
-var Home = Doc(
-	Markdown(`
-## Getting Started with QOR5
-
-This is how you start
-
+var Home = Doc(Markdown(`
+	QOR5 is the next gen of QOR3. with more flexibility.
 `),
 ).Title("QOR5 Documentation").
 	Slug("/").Tables(
 	ChildrenTable(
 		ContentGroup(
+			Doc(htmlgo.Text("placeholder")).Title("Introduction"),
+			Doc(htmlgo.Text("placeholder")).Title("Quick Start Guide"),
+		).Title("Getting Started"),
+		ContentGroup(
+			Doc(htmlgo.Text("Form fields")).Title("Form Field"),
+			Doc(htmlgo.Text("Form fields")).Title("Advanced Form Field"),
+			Doc(htmlgo.Text("Form fields")).Title("Action"),
+			Doc(htmlgo.Text("Form fields")).Title("Search & Filter & Scope"),
+		).Title("General Admin Configuration"),
+		ContentGroup(
 			Activity,
 			Slug,
 			SEO,
-		).Title("Packages"),
+		).Title("Addon Packages"),
 	),
 )
 
