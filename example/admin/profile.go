7a32f145f1103f133fb1fd5af932d1c96e1b4a8a
Zealllot <502372679@qq.com>
2022-04-16 00:38:25 +0800

    modify profile func

--- example/admin/profile.go
+++ example/admin/profile.go
@@ -1,33 +1,53 @@
 package admin
 
 import (
+	"strings"
+
 	"github.com/goplaid/web"
-	. "github.com/goplaid/x/vuetify"
+	"github.com/goplaid/x/vuetify"
 	"github.com/qor/qor5/example/models"
-	. "github.com/theplant/htmlgo"
+	h "github.com/theplant/htmlgo"
 )
 
-func profile(ctx *web.EventContext) HTMLComponent {
+func profile(ctx *web.EventContext) h.HTMLComponent {
 	u := getCurrentUser(ctx.R)
 	if u == nil {
-		return VBtn("Login").Text(true).Href("/auth/login")
+		return vuetify.VBtn("Login").Text(true).Href("/auth/login")
 	}
 
-	return VMenu().OffsetY(true).Children(
-		Template().Attr("v-slot:activator", "{on, attrs}").Children(
-			VAvatar().Class("ml-4").Color("secondary").Size(40).Attr("v-bind", "attrs").Attr("v-on", "on").Children(
-				If(u.AvatarURL == "",
-					Span(getAvatarShortName(u)).Class("white--text text-h5"),
-				).Else(
-					Img(u.AvatarURL).Alt(u.Name),
-				),
-			),
+	var roles []string
+	for _, role := range u.Roles {
+		roles = append(roles, role.Name)
+	}
+
+	return vuetify.VMenu().OffsetY(true).Children(
+		h.Template().Attr("v-slot:activator", "{on, attrs}").Children(
+			h.Div(
+				vuetify.VRow(
+					vuetify.VAvatar().Color("primary").Size(40).Children(
+						h.If(u.AvatarURL == "",
+							vuetify.VIcon("account_circle").Size(40),
+						).Else(
+							h.Img(u.AvatarURL).Alt(u.Name),
+						)),
+					h.Div(
+						h.Text(u.Name), h.If(len(u.Roles) > 0, h.Text("("+strings.Join(roles, ",")+")")),
+						h.Br(),
+						h.Text(u.Email),
+					).Style(`padding-left:5px;`),
+				)).Attr("v-bind", "attrs").Attr("v-on", "on"),
 		),
-		VList(
-			VListItem(
-				VListItemTitle(VBtn("Logout").Text(true).Href("/auth/logout")),
+		vuetify.VList(
+			h.Div(
+				vuetify.VListItem(
+					vuetify.VListItemContent(
+						vuetify.VListItemTitle(
+							h.Div(h.Text("Logout")).Class("text-button"),
+						),
+					),
+				).Attr("@click", web.Plaid().URL("/auth/logout").Go()),
 			),
-		),
+		).Dense(true),
 	)
 }
 
