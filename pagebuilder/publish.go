0ba33244e7080e1fedf1e78ea29e2e8daa453542
Azuma Daisuke <azuma@theplant.jp>
2022-07-19 15:34:25 +0800

    fix page publish

--- pagebuilder/publish.go
+++ pagebuilder/publish.go
@@ -57,7 +57,7 @@ func (p Page) getPublishUrl() string {
 
 func (p Page) getPublishContent(b *Builder, ctx context.Context) (r string, err error) {
 	w := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", fmt.Sprintf("/?id=%d", p.ID), nil)
+	req := httptest.NewRequest("GET", fmt.Sprintf("/?id=%d&version=%s", p.ID, p.GetVersion()), nil)
 	b.preview.ServeHTTP(w, req)
 
 	r = w.Body.String()
