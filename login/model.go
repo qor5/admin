764e9b5c29526e8676fe4801698fac7e867ce0aa
xuxin <xuxinmight@gmail.com>
2022-08-16 19:57:42 +0800

    add unique index for o_auth_indentifier

--- login/model.go
+++ login/model.go
@@ -46,12 +46,12 @@ type OAuthUser interface {
 }
 
 type OAuthInfo struct {
-	OAuthProvider string `gorm:"index:uidx_users_oauth,unique,where:o_auth_provider!='' and o_auth_user_id!=''"`
-	OAuthUserID   string `gorm:"index:uidx_users_oauth,unique,where:o_auth_provider!='' and o_auth_user_id!=''"`
+	OAuthProvider string `gorm:"index:uidx_users_oauth_provider_user_id,unique,where:o_auth_provider!='' and o_auth_user_id!='';index:uidx_users_oauth_provider_indentifier,unique,where:o_auth_provider!='' and o_auth_indentifier!=''"`
+	OAuthUserID   string `gorm:"index:uidx_users_oauth_provider_user_id,unique,where:o_auth_provider!='' and o_auth_user_id!=''"`
 	// the value that user can get to indentify his account
 	// in most cases is email or account name
 	// it is used to find the user record on the first login
-	OAuthIndentifier string
+	OAuthIndentifier string `gorm:"index:uidx_users_oauth_provider_indentifier,unique,where:o_auth_provider!='' and o_auth_indentifier!=''"`
 	OAuthAvatar      string `gorm:"-"`
 }
 
