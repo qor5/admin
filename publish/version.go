3d4f7242ca26423bfc3eeb789214b619fb1383cd
zealllot <502372679@qq.com>
2022-12-12 17:35:27 +0800

    add CreateVersion

--- publish/version.go
+++ publish/version.go
@@ -1,9 +1,11 @@
 package publish
 
 import (
+	"fmt"
 	"reflect"
 
 	"github.com/qor5/admin/utils"
+	"gorm.io/gorm"
 )
 
 type Version struct {
@@ -27,6 +29,20 @@ func (version Version) GetVersionName() string {
 
 func (version *Version) SetVersionName(v string) {
 	version.VersionName = v
+}
+
+func (version *Version) CreateVersion(db *gorm.DB, paramID string, obj interface{}) string {
+	date := db.NowFunc().Format("2006-01-02")
+	var count int64
+	utils.PrimarySluggerWhere(db.Unscoped(), obj, paramID, "version").
+		Where("version like ?", date+"%").
+		Order("version DESC").
+		Count(&count)
+
+	versionName := fmt.Sprintf("%s-v%02v", date, count+1)
+	version.Version = versionName
+	version.VersionName = versionName
+	return version.Version
 }
 
 func IsVersion(obj interface{}) (IsVersion bool) {
