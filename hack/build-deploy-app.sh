b4074d949beede1c16f5b9eb59168ae61e0f3d7e
yuan <yuan@theplant.jp>
2023-01-20 15:06:00 +0800

    deploy multiple images

--- hack/build-deploy-app.sh
+++ hack/build-deploy-app.sh
@@ -5,13 +5,22 @@ set -o nounset
 set -o pipefail
 
 # shellcheck disable=SC1091
-source hack/get-tools.sh
+#source hack/get-tools.sh
 
-plantbuild push ./plantbuild/build.jsonnet
+#plantbuild push ./plantbuild/build.jsonnet
 
 # https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#create-a-repository-dispatch-event
-curl -XPOST -H "Content-Type: application/json" \
-    -H "Accept:  application/vnd.github.everest-preview+json" \
-    -H "Authorization: Bearer $GITHUB_TOKEN" \
-    https://api.github.com/repos/theplant/qor5-provisioning/dispatches \
-    -d '{"event_type":"deploy-test","client_payload":{"github":{"example":{"image":"public.ecr.aws/qor5/example:latest"}}}}'
+#
+images=$(pb show ./plantbuild/build.jsonnet | jq -r .services[].image)
+set +e # ignore error so one failure doesn't stop the whole thing
+
+for image in $images; do
+  echo "deploying $image"
+
+  curl -XPOST -H "Content-Type: application/json" \
+      -H "Accept:  application/vnd.github.everest-preview+json" \
+      -H "Authorization: Bearer $GITHUB_TOKEN" \
+      https://api.github.com/repos/theplant/qor5-provisioning/dispatches \
+      -d '{"event_type":"deploy-test","client_payload":{"github":{"example":{"image":"'"$image"'"}}}}'
+done
+
