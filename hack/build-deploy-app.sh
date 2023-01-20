7cf30a90525829830fe0e1e4202a405621efde18
yuan <yuan@theplant.jp>
2023-01-20 14:05:18 +0800

    fix typo/add docs link/remove set-images

--- hack/build-deploy-app.sh
+++ hack/build-deploy-app.sh
@@ -8,10 +8,10 @@ set -o pipefail
 source hack/get-tools.sh
 
 plantbuild push ./plantbuild/build.jsonnet
-plantbuild k8s_set_images ./plantbuild/images.jsonnet
 
+# https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#create-a-repository-dispatch-event
 curl -XPOST -H "Content-Type: application/json" \
     -H "Accept:  application/vnd.github.everest-preview+json" \
     -H "Authorization: Bearer $GITHUB_TOKEN" \
     https://api.github.com/repos/theplant/qor5-provisioning/dispatches \
-    ∙ -d '{"event_type":"deploy-test","client_payload":{"github":{"example":{"image":"public.ecr.aws/qor5/example:latest"}}}}'
+    -d '{"event_type":"deploy-test","client_payload":{"github":{"example":{"image":"public.ecr.aws/qor5/example:latest"}}}}'
