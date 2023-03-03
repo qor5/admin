d2767e06423f80bb9eccd0ef3779c98b872f6751
Chenyang <moluuser@gmail.com>
2023-03-03 14:38:43 +0800

    Fixed init data on example

--- example/cmd/data-restorer/main.go
+++ example/cmd/data-restorer/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/qor5/admin/example/admin"
+	"github.com/qor5/admin/media/media_library"
 	"gorm.io/gorm"
 )
 
@@ -84,11 +87,33 @@ func initDB(db *gorm.DB) {
 		panic(err)
 	}
 	// Media Library
-	if err = db.Exec(initMediaLibrarySQL).Error; err != nil {
+	if err = db.Model(&media_library.MediaLibrary{}).Create(&[]map[string]interface{}{
+		{"id": 1, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"aigle.png","Url":"%s","Width":320,"Height":84,"FileSizes":{"@qor_preview":17065,"default":3159,"original":3159},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/1/file.png"))},
+		{"id": 2, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"asics.png","Url":"%s","Width":254,"Height":84,"FileSizes":{"@qor_preview":15571,"default":3060,"original":3060},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/2/file.png"))},
+		{"id": 3, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"file.20210903061739.png","Url":"%s","Width":1722,"Height":196,"FileSizes":{"@qor_preview":627,"default":6887,"original":6887},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/3/file.png"))},
+		{"id": 4, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"file.20211006224452.jpg","Url":"%s","Width":2880,"Height":720,"FileSizes":{"@qor_preview":19981,"default":257343,"original":257343},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/4/file.jpg"))},
+		{"id": 5, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"file.20211007041906.png","Url":"%s","Width":481,"Height":741,"FileSizes":{"@qor_preview":79999,"default":234306,"original":234306},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/5/file.png"))},
+		{"id": 6, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"file.20211007042027.png","Url":"%s","Width":481,"Height":741,"FileSizes":{"@qor_preview":65623,"default":203098,"original":203098},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/6/file.png"))},
+		{"id": 7, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"file.20211007042131.png","Url":"%s","Width":481,"Height":741,"FileSizes":{"@qor_preview":64838,"default":189979,"original":189979},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/7/file.png"))},
+		{"id": 8, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"file.20211007051449.png","Url":"%s","Width":2880,"Height":1097,"FileSizes":{"@qor_preview":75734,"default":2236473,"original":2236473},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/8/file.png"))},
+		{"id": 9, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"file.png","Url":"%s","Width":1252,"Height":658,"FileSizes":{"@qor_preview":41622,"default":227103,"original":227103},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/9/file.png"))},
+		{"id": 10, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"lacoste.png","Url":"%s","Width":470,"Height":84,"FileSizes":{"@qor_preview":11839,"default":4714,"original":4714},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/10/file.png"))},
+		{"id": 11, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"mob-mv.mov","Url":"%s","Video":"","SelectedType":"","Description":""}`, composeS3Path("/11/file.mov"))},
+		{"id": 12, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"mob.jpg","Url":"%s","Width":1536,"Height":2876,"FileSizes":{"@qor_preview":33140,"default":465542,"original":465542},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/12/file.jpg"))},
+		{"id": 13, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"nhk.png","Url":"%s","Width":202,"Height":84,"FileSizes":{"@qor_preview":14500,"default":2066,"original":2066},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/13/file.png"))},
+		{"id": 14, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"pc-mv.mov","Url":"%s","Video":"","SelectedType":"","Description":""}`, composeS3Path("/14/file.mov"))},
+		{"id": 15, "selected_type": "image", "file": fmt.Sprintf(`{"FileName":"pc.jpg","Url":"%s","Width":2560,"Height":1440,"FileSizes":{"@qor_preview":33019,"default":646542,"original":646542},"Video":"","SelectedType":"","Description":""}`, composeS3Path("/15/file.jpg"))},
+	}).Error; err != nil {
 		panic(err)
 	}
 	// Seq
 	if err = db.Exec(initSeqSQL).Error; err != nil {
 		panic(err)
 	}
+}
+
+// composeS3Path to generate file path as https://cdn.qor5.theplant-dev.com/system/media_libraries/236/file.jpeg
+func composeS3Path(filePath string) string {
+	endPoint := os.Getenv("S3_Endpoint")
+	return fmt.Sprintf("%s/system/media_libraries%s", endPoint, filePath)
 }
