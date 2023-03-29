bcb1d903b1ad5d1b89f6320082c0663878b737cb
zealllot <502372679@qq.com>
2023-03-29 15:37:13 +0800

    add RunPublisher

--- example/cmd/publisher/main.go
+++ example/cmd/publisher/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3control"
@@ -16,73 +13,14 @@ import (
 func main() {
 	db := admin.ConnectDB()
 	storage := s3.New(&s3.Config{
-		Bucket:  os.Getenv("S3_Publish_Bucket"),
-		Region:  os.Getenv("S3_Publish_Region"),
+		Bucket:  os.Getenv("S3_Bucket"),
+		Region:  os.Getenv("S3_Region"),
 		ACL:     s3control.S3CannedAccessControlListBucketOwnerFullControl,
 		Session: session.Must(session.NewSession()),
 	})
 	admin.NewConfig()
 
-	{ // schedule publisher
-		publisher := publish.New(db, storage)
-		scheduleP := publish.NewSchedulePublishBuilder(publisher)
-
-		for name, model := range publish.NonVersionPublishModels {
-			name := name
-			model := model
-			go RunJob("schedule-publisher"+"-"+name, time.Minute, time.Minute*5, func() {
-				if err := scheduleP.Run(model); err != nil {
-					panic(err)
-				}
-			})
-		}
-
-		for name, model := range publish.VersionPublishModels {
-			name := name
-			model := model
-			go RunJob("schedule-publisher"+"-"+name, time.Minute, time.Minute*5, func() {
-				if err := scheduleP.Run(model); err != nil {
-					panic(err)
-				}
-			})
-		}
-	}
-
-	{ // list publisher
-		listP := publish.NewListPublishBuilder(db, storage)
-		for name, model := range publish.ListPublishModels {
-			name := name
-			model := model
-			go RunJob("list-publisher"+"-"+name, time.Minute, time.Minute*5, func() {
-				if err := listP.Run(model); err != nil {
-					panic(err)
-				}
-			})
-		}
-	}
+	publish.RunPublisher(db, storage)
 
 	select {}
-}
-
-func RunJob(jobName string, interval time.Duration, timeout time.Duration, f func()) {
-	t := time.Tick(interval)
-	for range t {
-		start := time.Now()
-		s := make(chan bool, 1)
-		go func() {
-			defer func() {
-				stop := time.Now()
-				log.Printf("job_name: %s, started_at: %s, stopped_at: %s, time_spent_ms: %s\n", jobName, start, stop, fmt.Sprintf("%f", float64(stop.Sub(start))/float64(time.Millisecond)))
-			}()
-			f()
-			s <- true
-		}()
-
-		select {
-		case <-s:
-		case <-time.After(timeout):
-			log.Printf("job_name: %s, started_at: %s, timeout: %s\n", jobName, start, time.Now())
-			os.Exit(124)
-		}
-	}
 }
---
+++ publish/utils.go
@@ -0,0 +1,74 @@
+package publish
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/qor/oss"
+	"gorm.io/gorm"
+)
+
+func RunPublisher(db *gorm.DB, storage oss.StorageInterface) {
+	{ // schedule publisher
+		publisher := New(db, storage)
+		scheduleP := NewSchedulePublishBuilder(publisher)
+
+		for name, model := range NonVersionPublishModels {
+			name := name
+			model := model
+			go RunJob("schedule-publisher"+"-"+name, time.Minute, time.Minute*5, func() {
+				if err := scheduleP.Run(model); err != nil {
+					panic(err)
+				}
+			})
+		}
+
+		for name, model := range VersionPublishModels {
+			name := name
+			model := model
+			go RunJob("schedule-publisher"+"-"+name, time.Minute, time.Minute*5, func() {
+				if err := scheduleP.Run(model); err != nil {
+					panic(err)
+				}
+			})
+		}
+	}
+
+	{ // list publisher
+		listP := NewListPublishBuilder(db, storage)
+		for name, model := range ListPublishModels {
+			name := name
+			model := model
+			go RunJob("list-publisher"+"-"+name, time.Minute, time.Minute*5, func() {
+				if err := listP.Run(model); err != nil {
+					panic(err)
+				}
+			})
+		}
+	}
+}
+
+func RunJob(jobName string, interval time.Duration, timeout time.Duration, f func()) {
+	t := time.Tick(interval)
+	for range t {
+		start := time.Now()
+		s := make(chan bool, 1)
+		go func() {
+			defer func() {
+				stop := time.Now()
+				log.Printf("job_name: %s, started_at: %s, stopped_at: %s, time_spent_ms: %s\n", jobName, start, stop, fmt.Sprintf("%f", float64(stop.Sub(start))/float64(time.Millisecond)))
+			}()
+			f()
+			s <- true
+		}()
+
+		select {
+		case <-s:
+		case <-time.After(timeout):
+			log.Printf("job_name: %s, started_at: %s, timeout: %s\n", jobName, start, time.Now())
+			os.Exit(124)
+		}
+	}
+}
