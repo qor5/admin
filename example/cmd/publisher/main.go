72d6e231a20ec3bc372febd442303b7053b1fcc9
Chenyang <moluuser@gmail.com>
2023-04-20 23:05:47 +0800

    Optimize scheduled publishing and make time more accurate

--- example/cmd/publisher/main.go
+++ example/cmd/publisher/main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"os"
-
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3control"
-	"github.com/qor/oss/s3"
 	"github.com/qor5/admin/example/admin"
 	"github.com/qor5/admin/publish"
 )
 
 func main() {
 	db := admin.ConnectDB()
-	storage := s3.New(&s3.Config{
-		Bucket:  os.Getenv("S3_Publish_Bucket"),
-		Region:  os.Getenv("S3_Publish_Region"),
-		ACL:     s3control.S3CannedAccessControlListBucketOwnerFullControl,
-		Session: session.Must(session.NewSession()),
-	})
 	config := admin.NewConfig()
-
+	storage := admin.PublishStorage
 	publish.RunPublisher(db, storage, config.Publisher)
-
 	select {}
 }
