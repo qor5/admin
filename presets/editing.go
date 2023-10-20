96562a970729bb69d814dc9e2e02b49717a8a75c
iBakuman <67883044+iBakuman@users.noreply.github.com>
2023-10-20 16:32:16 +0800

    fix the issue where the version name does not update immediately after updating its value
    https://theplanttokyo.atlassian.net/browse/QOR5-194

--- presets/editing.go
+++ presets/editing.go
@@ -280,8 +280,7 @@ func (b *EditingBuilder) editFormFor(obj interface{}, ctx *web.EventContext) h.H
 		VCardActions(actionButtons),
 	)
 
-	var asideContent h.HTMLComponent = formContent
-
+	var asideContent h.HTMLComponent
 	if len(b.tabPanels) != 0 {
 		var tabs []h.HTMLComponent
 
@@ -299,6 +298,8 @@ func (b *EditingBuilder) editFormFor(obj interface{}, ctx *web.EventContext) h.H
 				h.Components(tabs...),
 			).Class("v-tabs--fixed-tabs")
 		}
+	} else {
+		asideContent = web.Scope(formContent).VSlot("{plaidForm}")
 	}
 
 	if b.sidePanel != nil {
