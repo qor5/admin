11fe4255d88d56762b3aff2267b2b3162e6befd3
Lewis <luyiguo86@gmail.com>
2024-03-28 18:29:54 +0800

    add temporary icon.go

---
+++ presets/icons.go
@@ -0,0 +1,55 @@
+package presets
+
+import h "github.com/theplant/htmlgo"
+
+var languageSwitchIcon = h.RawHTML(`
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clip-path="url(#clip0_2_4948)">
+<mask id="mask0_2_4948" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+<rect width="24" height="24" fill="white"/>
+</mask>
+<g mask="url(#mask0_2_4948)">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M0 0V24H24V0H0Z" fill="#2E42A5"/>
+<mask id="mask1_2_4948" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M0 0V24H24V0H0Z" fill="white"/>
+</mask>
+<g mask="url(#mask1_2_4948)">
+<mask id="mask2_2_4948" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+<rect width="24" height="24" fill="white"/>
+</mask>
+<g mask="url(#mask2_2_4948)">
+<path d="M-2.67236 22.2854L2.6089 25.2635L24.1198 3.23787L26.9056 -1.18761L21.258 -2.18297L12.4842 7.3085L5.42223 13.7035L-2.67236 22.2854Z" fill="white"/>
+<path d="M-1.94934 24.3719L0.741221 26.1001L25.9052 -1.59881H22.1274L-1.94934 24.3719Z" fill="#F50100"/>
+<path d="M26.6724 22.2854L21.3911 25.2635L-0.119832 3.23787L-2.90558 -1.18761L2.74197 -2.18297L11.5158 7.3085L18.5778 13.7035L26.6724 22.2854Z" fill="white"/>
+<path d="M26.4922 23.7829L23.8016 25.5111L13.0865 13.6518L9.90973 12.3267L-3.1736 -1.17246H0.604259L13.6802 12.0063L17.1535 13.5952L26.4922 23.7829Z" fill="#F50100"/>
+<mask id="path-10-inside-1_2_4948" fill="white">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M14.8333 -2H9.16665V8H-1.47937V16H9.16665V26H14.8333V16H25.5206V8H14.8333V-2Z"/>
+</mask>
+<path fill-rule="evenodd" clip-rule="evenodd" d="M14.8333 -2H9.16665V8H-1.47937V16H9.16665V26H14.8333V16H25.5206V8H14.8333V-2Z" fill="#F50100"/>
+<path d="M9.16665 -2V-4H7.16665V-2H9.16665ZM14.8333 -2H16.8333V-4H14.8333V-2ZM9.16665 8V10H11.1666V8H9.16665ZM-1.47937 8V6H-3.47937V8H-1.47937ZM-1.47937 16H-3.47937V18H-1.47937V16ZM9.16665 16H11.1666V14H9.16665V16ZM9.16665 26H7.16665V28H9.16665V26ZM14.8333 26V28H16.8333V26H14.8333ZM14.8333 16V14H12.8333V16H14.8333ZM25.5206 16V18H27.5206V16H25.5206ZM25.5206 8H27.5206V6H25.5206V8ZM14.8333 8H12.8333V10H14.8333V8ZM9.16665 0H14.8333V-4H9.16665V0ZM11.1666 8V-2H7.16665V8H11.1666ZM-1.47937 10H9.16665V6H-1.47937V10ZM0.52063 16V8H-3.47937V16H0.52063ZM9.16665 14H-1.47937V18H9.16665V14ZM11.1666 26V16H7.16665V26H11.1666ZM14.8333 24H9.16665V28H14.8333V24ZM12.8333 16V26H16.8333V16H12.8333ZM25.5206 14H14.8333V18H25.5206V14ZM23.5206 8V16H27.5206V8H23.5206ZM14.8333 10H25.5206V6H14.8333V10ZM12.8333 -2V8H16.8333V-2H12.8333Z" fill="white" mask="url(#path-10-inside-1_2_4948)"/>
+</g>
+</g>
+</g>
+</g>
+<rect width="24" height="24" rx="12" fill="url(#paint0_radial_2_4948)" style="mix-blend-mode:overlay"/>
+<rect width="24" height="24" rx="12" fill="url(#paint1_linear_2_4948)" style="mix-blend-mode:overlay"/>
+<rect x="0.5" y="0.5" width="23" height="23" rx="11.5" stroke="black" stroke-opacity="0.1"/>
+<defs>
+<radialGradient id="paint0_radial_2_4948" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(12 3.5) rotate(90) scale(27.5 25.795)">
+<stop stop-color="white" stop-opacity="0.3"/>
+<stop offset="0.289252" stop-opacity="0.24"/>
+<stop offset="0.46298" stop-opacity="0.55"/>
+<stop offset="0.605698" stop-color="white" stop-opacity="0.26"/>
+<stop offset="0.726705" stop-opacity="0.17"/>
+<stop offset="1" stop-color="white" stop-opacity="0.3"/>
+</radialGradient>
+<linearGradient id="paint1_linear_2_4948" x1="12" y1="0" x2="12" y2="24" gradientUnits="userSpaceOnUse">
+<stop stop-color="white" stop-opacity="0.7"/>
+<stop offset="1" stop-opacity="0.3"/>
+</linearGradient>
+<clipPath id="clip0_2_4948">
+<rect width="24" height="24" rx="12" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+`)
