db6df70be3a1b6f39af54fee3ce0f5981072eb48
jsthon <jsthon@live.com>
2024-04-25 15:06:05 +0800

    Add variables for light theme

--- example/admin/config.go
+++ example/admin/config.go
@@ -80,21 +80,101 @@ func NewConfig() Config {
 	}))
 	b := presets.New().RightDrawerWidth("700").VuetifyOptions(`
 {
-  icons: {
-	iconfont: 'md', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4'
-  },
   theme: {
+    defaultTheme: 'light',
     themes: {
-      light: {
-		  primary:   "#3E63DD",
-		  secondary: "#5B6471",
-		  accent:    "#82B1FF",
-		  error:     "#82B1FF",
-		  info:      "#0091FF",
-		  success:   "#30A46C",
-		  warning:   "#F76808",
-      },
-    },
+	  light: {
+		dark: false,
+		colors: {
+		  // primary
+		  'primary': '#3E63DD',
+		  'on-primary': '#fff',
+		  'primary-darken-1': '#3451B2',
+		  'primary-darken-2': '#101D46',
+		  'primary-lighten-1': '#8DA4EF',
+		  'primary-lighten-2': '#E6EDFE',
+		  // secondary
+		  'secondary': '#5B6471',
+		  'on-secondary': '#fff',
+		  'secondary-darken-1': '#444C58',
+		  'secondary-darken-2': '#19212B',
+		  'secondary-lighten-1': '#C2CDDC',
+		  'secondary-lighten-2': '#E4ECFE',
+		  // info
+		  'info': '#0091FF',
+		  'on-info': '#fff',
+		  'info-darken-1': '#006ADC',
+		  'info-darken-2': '#00254D',
+		  'info-lighten-1': '#5EB0EF',
+		  'info-lighten-2': '#E1F0FF',
+		  // success
+		  'success': '#30A46C',
+		  'on-success': '#fff',
+		  'success-darken-1': '#18794E',
+		  'success-darken-2': '#153226',
+		  'success-lighten-1': '#5BB98C',
+		  'success-lighten-2': '#DDF3E4',
+		  // warning
+		  'warning': '#F76808',
+		  'on-warning': '#fff',
+		  'warning-darken-1': '#BD4B00',
+		  'warning-darken-2': '#451E11',
+		  'warning-lighten-1': '#FA934E',
+		  'warning-lighten-2': '#FFE8D7',
+		  // error
+		  'error': '#E5484D',
+		  'on-error': '#fff',
+		  'error-darken-1': '#CD2B31',
+		  'error-darken-2': '#381316',
+		  'error-lighten-1': '#EB9091',
+		  'error-lighten-2': '#FFE5E5',
+		  // background
+		  'background': '#fff',
+		  'on-background': '#212121',
+		  // surface
+		  'surface': '#fff',
+		  'on-surface': '#424242',
+		  // grey
+		  'grey-50': '#FAFAFA',
+		  'grey-100': '#F5F5F5',
+		  'grey-200': '#EEEEEE',
+		  'grey-300': '#E0E0E0',
+		  'grey-400': '#BDBDBD',
+		  'grey-500': '#9E9E9E',
+		  'grey-600': '#757575',
+		  'grey-700': '#616161',
+		  'grey-800': '#424242',
+		  'grey-900': '#212121',
+		},
+		variables: {
+		  'border-color': '#000000',
+		  'border-opacity': 0.12,
+		  'high-emphasis-opacity': 1,
+		  'medium-emphasis-opacity': 0.60,
+		  'disabled-opacity': 0.38,
+		  'idle-opacity': 0.04,
+		  'hover-opacity': 0.04,
+		  'focus-opacity': 0.12,
+		  'selected-opacity': 0.08,
+		  'activated-opacity': 0.12,
+		  'pressed-opacity': 0.12,
+		  'dragged-opacity': 0.08,
+		  'theme-kbd': '#212121',
+		  'theme-on-kbd': '#FFFFFF',
+		  'theme-code': '#F5F5F5',
+		  'theme-on-code': '#000000',
+	
+		  /* custom variables */
+		  // shadows
+		  'shadow-key-umbra-color': '#000000',
+		  'shadow-xs-opacity': '0.16',
+		  'shadow-sm-opacity': '0.18',
+		  'shadow-md-opacity': '0.20',
+		  'shadow-lg-opacity': '0.22',
+		  'shadow-xl-opacity': '0.24',
+		},
+	  },
+	},
   },
 }
 `)
