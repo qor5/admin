86603ec0e123a509121b70bb4a557decc538cb8c
Felix Sun <sunfmin@gmail.com>
2024-06-12 19:08:20 +0800

    Add Test

--- docs/docsrc/examples/examples_admin/activity_test.go
+++ docs/docsrc/examples/examples_admin/activity_test.go
@@ -1,8 +1,11 @@
 package examples_admin
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/qor5/admin/v3/presets"
@@ -18,10 +21,21 @@ INSERT INTO public.with_activity_products (title, code, price, id, created_at, u
 
 `, []string{"with_activity_products", "activity_logs"}))
 
+func mockHandleError(in http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				http.Error(w, fmt.Sprint(r), http.StatusInternalServerError)
+			}
+		}()
+		in.ServeHTTP(w, r)
+	})
+}
+
 func TestActivity(t *testing.T) {
 	pb := presets.New().DataOperator(gorm2op.DataOperator(TestDB))
 	ActivityExample(pb, TestDB)
-
+	h := mockHandleError(pb)
 	dbr, _ := TestDB.DB()
 
 	cases := []multipartestutils.TestCase{
@@ -46,11 +60,45 @@ func TestActivity(t *testing.T) {
 			},
 			ExpectPortalUpdate0ContainsInOrder: []string{"WithActivityProduct 1"},
 		},
+		{
+			Name:  "create note",
+			Debug: true,
+			ReqFunc: func() *http.Request {
+				activityData.TruncatePut(dbr)
+				req := multipartestutils.NewMultipartBuilder().
+					PageURL("/with-activity-products?__execute_event__=note_CreateNoteEvent").
+					AddField("resource_id", "1").
+					AddField("resource_type", "WithActivityProduct").
+					AddField("Content", "Hello content, I am writing a content").
+					BuildEventFuncRequest()
+				return req
+			},
+			ExpectPortalUpdate0ContainsInOrder: []string{"Hello content, I am writing a content"},
+		},
+		{
+			Name:  "create note with invalid data",
+			Debug: true,
+			ReqFunc: func() *http.Request {
+				activityData.TruncatePut(dbr)
+				req := multipartestutils.NewMultipartBuilder().
+					PageURL("/with-activity-products?__execute_event__=note_CreateNoteEvent").
+					AddField("resource_type", "WithActivityProduct").
+					AddField("Content", "Hello content, I am writing a content").
+					BuildEventFuncRequest()
+				return req
+			},
+			PageMatch: func(t *testing.T, body *bytes.Buffer) {
+				fmt.Println(body.String())
+				if !strings.Contains(body.String(), "Missing required parameter") {
+					t.Error("didn't check correct")
+				}
+			},
+		},
 	}
 
 	for _, c := range cases {
 		t.Run(c.Name, func(t *testing.T) {
-			multipartestutils.RunCase(t, c, pb)
+			multipartestutils.RunCase(t, c, h)
 		})
 	}
 }
