31ccb3eba926ac410ebf336ce80c52fe26337b41
molon <3739161+molon@users.noreply.github.com>
2024-07-17 16:53:38 +0800

    activity: add PerformActionNoDetailTemplate

--- activity/messages.go
+++ activity/messages.go
@@ -38,31 +38,32 @@ type Messages struct {
 	DiffNew     string
 	DiffValue   string
 
-	AddedANote              string
-	LastEditedAtTemplate    string
-	EditedNFieldsTemplate   string
-	MoreInfo                string
-	Created                 string
-	Viewed                  string
-	Deleted                 string
-	PerformActionTemplate   string
-	AddNote                 string
-	UnknownCreator          string
-	NoteCannotBeEmpty       string
-	FailedToCreateNote      string
-	SuccessfullyCreatedNote string
-	FailedToGetCurrentUser  string
-	FailedToGetNote         string
-	YouAreNotTheNoteCreator string
-	FailedToUpdateNote      string
-	SuccessfullyUpdatedNote string
-	FailedToDeleteNote      string
-	SuccessfullyDeletedNote string
-	DeleteNoteDialogTitle   string
-	DeleteNoteDialogText    string
-	Cancel                  string
-	Delete                  string
-	NoActivitiesYet         string
+	AddedANote                    string
+	LastEditedAtTemplate          string
+	EditedNFieldsTemplate         string
+	MoreInfo                      string
+	Created                       string
+	Viewed                        string
+	Deleted                       string
+	PerformActionNoDetailTemplate string
+	PerformActionTemplate         string
+	AddNote                       string
+	UnknownCreator                string
+	NoteCannotBeEmpty             string
+	FailedToCreateNote            string
+	SuccessfullyCreatedNote       string
+	FailedToGetCurrentUser        string
+	FailedToGetNote               string
+	YouAreNotTheNoteCreator       string
+	FailedToUpdateNote            string
+	SuccessfullyUpdatedNote       string
+	FailedToDeleteNote            string
+	SuccessfullyDeletedNote       string
+	DeleteNoteDialogTitle         string
+	DeleteNoteDialogText          string
+	Cancel                        string
+	Delete                        string
+	NoActivitiesYet               string
 }
 
 func (msgr *Messages) LastEditedAt(desc string) string {
@@ -76,6 +77,11 @@ func (msgr *Messages) EditedNFields(n int) string {
 }
 
 func (msgr *Messages) PerformAction(action string, detail string) string {
+	if detail == "" || detail == "null" || detail == "{}" {
+		return strings.NewReplacer(
+			"{action}", action,
+		).Replace(msgr.PerformActionNoDetailTemplate)
+	}
 	return strings.NewReplacer(
 		"{action}", action,
 		"{detail}", detail,
@@ -115,31 +121,32 @@ var Messages_en_US = &Messages{
 	DiffNew:     "Now",
 	DiffValue:   "Value",
 
-	AddedANote:              "Added a note :",
-	LastEditedAtTemplate:    "(edited at {desc})",
-	EditedNFieldsTemplate:   "Edited {n} fields",
-	MoreInfo:                "More Info",
-	Created:                 "Created",
-	Viewed:                  "Viewed",
-	Deleted:                 "Deleted",
-	PerformActionTemplate:   "Perform {action} with {detail}",
-	AddNote:                 "Add Note",
-	UnknownCreator:          "Unknown",
-	NoteCannotBeEmpty:       "Note cannot be empty",
-	FailedToCreateNote:      "Failed to create note",
-	SuccessfullyCreatedNote: "Successfully created note",
-	FailedToGetCurrentUser:  "Failed to get current user",
-	FailedToGetNote:         "Failed to get note",
-	YouAreNotTheNoteCreator: "You are not the creator of this note",
-	FailedToUpdateNote:      "Failed to update note",
-	SuccessfullyUpdatedNote: "Successfully updated note",
-	FailedToDeleteNote:      "Failed to delete note",
-	SuccessfullyDeletedNote: "Successfully deleted note",
-	DeleteNoteDialogTitle:   "Delete Note",
-	DeleteNoteDialogText:    "Are you sure you want to delete this note?",
-	Cancel:                  "Cancel",
-	Delete:                  "Delete",
-	NoActivitiesYet:         "No activities yet",
+	AddedANote:                    "Added a note :",
+	LastEditedAtTemplate:          "(edited at {desc})",
+	EditedNFieldsTemplate:         "Edited {n} fields",
+	MoreInfo:                      "More Info",
+	Created:                       "Created",
+	Viewed:                        "Viewed",
+	Deleted:                       "Deleted",
+	PerformActionNoDetailTemplate: "Perform {action}",
+	PerformActionTemplate:         "Perform {action} with {detail}",
+	AddNote:                       "Add Note",
+	UnknownCreator:                "Unknown",
+	NoteCannotBeEmpty:             "Note cannot be empty",
+	FailedToCreateNote:            "Failed to create note",
+	SuccessfullyCreatedNote:       "Successfully created note",
+	FailedToGetCurrentUser:        "Failed to get current user",
+	FailedToGetNote:               "Failed to get note",
+	YouAreNotTheNoteCreator:       "You are not the creator of this note",
+	FailedToUpdateNote:            "Failed to update note",
+	SuccessfullyUpdatedNote:       "Successfully updated note",
+	FailedToDeleteNote:            "Failed to delete note",
+	SuccessfullyDeletedNote:       "Successfully deleted note",
+	DeleteNoteDialogTitle:         "Delete Note",
+	DeleteNoteDialogText:          "Are you sure you want to delete this note?",
+	Cancel:                        "Cancel",
+	Delete:                        "Delete",
+	NoActivitiesYet:               "No activities yet",
 }
 
 var Messages_zh_CN = &Messages{
@@ -174,31 +181,32 @@ var Messages_zh_CN = &Messages{
 	DiffNew:         "当前的值",
 	DiffValue:       "值",
 
-	AddedANote:              "添加了一个备注：",
-	LastEditedAtTemplate:    "编辑于 {desc}",
-	EditedNFieldsTemplate:   "编辑了 {n} 个字段",
-	MoreInfo:                "更多信息",
-	Created:                 "创建",
-	Viewed:                  "查看",
-	Deleted:                 "删除",
-	PerformActionTemplate:   "执行 {action} 操作，详情为 {detail}",
-	AddNote:                 "添加备注",
-	UnknownCreator:          "未知",
-	NoteCannotBeEmpty:       "备注不能为空",
-	FailedToCreateNote:      "创建备注失败",
-	SuccessfullyCreatedNote: "成功创建备注",
-	FailedToGetCurrentUser:  "获取当前用户失败",
-	FailedToGetNote:         "获取备注失败",
-	YouAreNotTheNoteCreator: "您不是备注的创建者",
-	FailedToUpdateNote:      "更新备注失败",
-	SuccessfullyUpdatedNote: "成功更新备注",
-	FailedToDeleteNote:      "删除备注失败",
-	SuccessfullyDeletedNote: "成功删除备注",
-	DeleteNoteDialogTitle:   "删除备注",
-	DeleteNoteDialogText:    "确定要删除此备注吗？",
-	Cancel:                  "取消",
-	Delete:                  "删除",
-	NoActivitiesYet:         "暂无活动",
+	AddedANote:                    "添加了一个备注：",
+	LastEditedAtTemplate:          "编辑于 {desc}",
+	EditedNFieldsTemplate:         "编辑了 {n} 个字段",
+	MoreInfo:                      "更多信息",
+	Created:                       "创建",
+	Viewed:                        "查看",
+	Deleted:                       "删除",
+	PerformActionNoDetailTemplate: "执行 {action}",
+	PerformActionTemplate:         "执行 {action} 操作，详情为 {detail}",
+	AddNote:                       "添加备注",
+	UnknownCreator:                "未知",
+	NoteCannotBeEmpty:             "备注不能为空",
+	FailedToCreateNote:            "创建备注失败",
+	SuccessfullyCreatedNote:       "成功创建备注",
+	FailedToGetCurrentUser:        "获取当前用户失败",
+	FailedToGetNote:               "获取备注失败",
+	YouAreNotTheNoteCreator:       "您不是备注的创建者",
+	FailedToUpdateNote:            "更新备注失败",
+	SuccessfullyUpdatedNote:       "成功更新备注",
+	FailedToDeleteNote:            "删除备注失败",
+	SuccessfullyDeletedNote:       "成功删除备注",
+	DeleteNoteDialogTitle:         "删除备注",
+	DeleteNoteDialogText:          "确定要删除此备注吗？",
+	Cancel:                        "取消",
+	Delete:                        "删除",
+	NoActivitiesYet:               "暂无活动",
 }
 
 var Messages_ja_JP = &Messages{
@@ -234,29 +242,30 @@ var Messages_ja_JP = &Messages{
 	DiffNew:     "現在",
 	DiffValue:   "値",
 
-	AddedANote:              "ノートを追加しました：",
-	LastEditedAtTemplate:    "{desc} に編集",
-	EditedNFieldsTemplate:   "{n}つのフィールドを編集しました",
-	MoreInfo:                "詳細情報",
-	Created:                 "作成",
-	Viewed:                  "表示",
-	Deleted:                 "削除",
-	PerformActionTemplate:   "{action}を実行し、{detail}を使用",
-	AddNote:                 "ノートを追加",
-	UnknownCreator:          "不明",
-	NoteCannotBeEmpty:       "ノートは空にできません",
-	FailedToCreateNote:      "ノートの作成に失敗しました",
-	SuccessfullyCreatedNote: "ノートの作成に成功しました",
-	FailedToGetCurrentUser:  "現在のユーザーの取得に失敗しました",
-	FailedToGetNote:         "ノートの取得に失敗しました",
-	YouAreNotTheNoteCreator: "このノートの作成者ではありません",
-	FailedToUpdateNote:      "ノートの更新に失敗しました",
-	SuccessfullyUpdatedNote: "ノートの更新に成功しました",
-	FailedToDeleteNote:      "ノートの削除に失敗しました",
-	SuccessfullyDeletedNote: "ノートの削除に成功しました",
-	DeleteNoteDialogTitle:   "ノートを削除",
-	DeleteNoteDialogText:    "このノートを削除してもよろしいですか？",
-	Cancel:                  "キャンセル",
-	Delete:                  "削除",
-	NoActivitiesYet:         "まだアクティビティはありません",
+	AddedANote:                    "ノートを追加しました：",
+	LastEditedAtTemplate:          "{desc} に編集",
+	EditedNFieldsTemplate:         "{n}つのフィールドを編集しました",
+	MoreInfo:                      "詳細情報",
+	Created:                       "作成",
+	Viewed:                        "表示",
+	Deleted:                       "削除",
+	PerformActionNoDetailTemplate: "{action} を実行",
+	PerformActionTemplate:         "{action} を実行し、{detail} を使用",
+	AddNote:                       "ノートを追加",
+	UnknownCreator:                "不明",
+	NoteCannotBeEmpty:             "ノートは空にできません",
+	FailedToCreateNote:            "ノートの作成に失敗しました",
+	SuccessfullyCreatedNote:       "ノートの作成に成功しました",
+	FailedToGetCurrentUser:        "現在のユーザーの取得に失敗しました",
+	FailedToGetNote:               "ノートの取得に失敗しました",
+	YouAreNotTheNoteCreator:       "このノートの作成者ではありません",
+	FailedToUpdateNote:            "ノートの更新に失敗しました",
+	SuccessfullyUpdatedNote:       "ノートの更新に成功しました",
+	FailedToDeleteNote:            "ノートの削除に失敗しました",
+	SuccessfullyDeletedNote:       "ノートの削除に成功しました",
+	DeleteNoteDialogTitle:         "ノートを削除",
+	DeleteNoteDialogText:          "このノートを削除してもよろしいですか？",
+	Cancel:                        "キャンセル",
+	Delete:                        "削除",
+	NoActivitiesYet:               "まだアクティビティはありません",
 }
