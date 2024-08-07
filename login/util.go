8cdf2accc503689ed5fb887c5ecc9a12f874ba99
molon <3739161+molon@users.noreply.github.com>
2024-08-07 13:52:23 +0800

    profile: optional SessionBuilder and DisableNotification and use first upper word

--- login/util.go
+++ login/util.go
@@ -47,19 +47,19 @@ func proxy(r *http.Request) []string {
 	return nil
 }
 
-func shortName(name string) string {
-	if name == "" {
-		return ""
-	}
-	runes := []rune(name)
-	result := strings.ToUpper(string(runes[0:1]))
-	if len(runes) > 2 {
-		for i := 2; i < len(runes); i++ {
-			if runes[i-1] == ' ' && runes[i] != ' ' {
-				result += strings.ToUpper(string(runes[i : i+1]))
-				break
-			}
-		}
-	}
-	return result
-}
+// func shortName(name string) string {
+// 	if name == "" {
+// 		return ""
+// 	}
+// 	runes := []rune(name)
+// 	result := strings.ToUpper(string(runes[0:1]))
+// 	if len(runes) > 2 {
+// 		for i := 2; i < len(runes); i++ {
+// 			if runes[i-1] == ' ' && runes[i] != ' ' {
+// 				result += strings.ToUpper(string(runes[i : i+1]))
+// 				break
+// 			}
+// 		}
+// 	}
+// 	return result
+// }
