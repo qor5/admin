88dcd2a5a0ddc8a7536d141ee10ba8c22e007667
Daniel <contact@danni.cool>
2024-08-13 15:27:17 +0800

    style: the adjusting of edit btn click area

--- presets/section.go
+++ presets/section.go
@@ -389,7 +389,7 @@ func (b *SectionBuilder) viewComponent(obj interface{}, field *FieldContext, ctx
 	}
 
 	disableEditBtn := b.father.mb.Info().Verifier().Do(PermUpdate).ObjectOn(obj).WithReq(ctx.R).IsAllowed() != nil
-	btn := VBtn(i18n.T(ctx.R, CoreI18nModuleKey, "Edit")).Size(SizeXSmall).Variant("text").
+	btn := VBtn(i18n.T(ctx.R, CoreI18nModuleKey, "Edit")).Variant("text").
 		PrependIcon("mdi-pencil-outline").
 		Attr("v-show", fmt.Sprintf("%t&&%t", b.componentEditBtnFunc(obj, ctx), !disableEditBtn)).
 		Attr("@click", web.Plaid().
