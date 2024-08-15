1c536c81d0e005088fba705a5e60910573d67495
molon <3739161+molon@users.noreply.github.com>
2024-08-15 16:07:21 +0800

    pagebuilder: use history.back for back btn

--- pagebuilder/builder.go
+++ pagebuilder/builder.go
@@ -473,7 +473,7 @@ func (b *Builder) defaultPageInstall(pb *presets.Builder, pm *presets.ModelBuild
 
 		return h.Div(
 			VBtn("").Size(SizeXSmall).Icon("mdi-arrow-left").Tile(true).Variant(VariantOutlined).Attr("@click",
-				web.GET().URL(pm.Info().ListingHref()).PushState(true).Go(),
+				fmt.Sprintf(`$event.view.window.history.length <= 2 ? %s : $event.view.window.history.back()`, web.GET().URL(pm.Info().ListingHref()).PushState(true).Go()),
 			),
 			h.H1("{{vars.pageTitle}}").Class("ml-4"),
 			versionBadge.Class("mt-2 ml-2"),
@@ -1376,6 +1376,7 @@ func (b *ContainerBuilder) OnlyPages(v bool) *ContainerBuilder {
 	b.onlyPages = v
 	return b
 }
+
 func (b *ContainerBuilder) uRIName(uri string) *ContainerBuilder {
 	if b.mb == nil {
 		return b
