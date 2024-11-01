1a2e5c15a6bd71c4dab611b8a4d7e686cc74b05f
molon <3739161+molon@users.noreply.github.com>
2024-11-01 14:58:27 +0800

    text-wrap for pre element

--- activity/admin.go
+++ activity/admin.go
@@ -355,7 +355,7 @@ func setupDetailing(dp *presets.DetailingBuilder, op *gorm2op.DataOperatorBuilde
 						),
 						VCardText().Class("mt-3 pa-3 border-thin rounded").Children(
 							h.Div().Class("d-flex flex-column").Children(
-								h.Pre(note.Note).Attr("v-pre", true).Class("text-body-2"),
+								h.Pre(note.Note).Attr("v-pre", true).Class("text-body-2 text-wrap"),
 								h.Iff(!note.LastEditedAt.IsZero(), func() h.HTMLComponent {
 									return h.Div().Class("text-caption font-italic").Style("color: #757575").Children(
 										h.Text(msgr.LastEditedAt(pmsgr.HumanizeTime(note.LastEditedAt))),
