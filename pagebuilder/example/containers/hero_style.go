d511ca91d692fed0d2a77824c9e99a6fddaa80b7
Daniel <contact@danni.cool>
2024-12-13 19:04:30 +0800

    feat: enhance hero style component with custom layout buttons for improved UI interaction

--- pagebuilder/example/containers/hero_style.go
+++ pagebuilder/example/containers/hero_style.go
@@ -9,6 +9,9 @@ import (
 	"github.com/qor5/admin/v3/pagebuilder"
 	"github.com/qor5/admin/v3/presets"
 	"github.com/qor5/web/v3"
+	v "github.com/qor5/x/v3/ui/vuetify"
+	vx "github.com/qor5/x/v3/ui/vuetifyx"
+	"github.com/sunfmin/reflectutils"
 	. "github.com/theplant/htmlgo"
 )
 
@@ -39,7 +42,57 @@ func SetHeroStyleComponent(pb *pagebuilder.Builder, eb *presets.EditingBuilder) 
 	fb := pb.GetPresetsBuilder().NewFieldsBuilder(presets.WRITE).Model(&heroStyle{}).Only("Layout", "TopSpace", "BottomSpace", "ImageBackground")
 
 	fb.Field("Layout").ComponentFunc(func(obj interface{}, field *presets.FieldContext, ctx *web.EventContext) HTMLComponent {
-		return presets.SelectField(obj, field, ctx).Items([]string{"left", "right"})
+		// return presets.SelectField(obj, field, ctx).Items([]string{"left", "right"})
+
+		return Div(
+			vx.VXLabel(Text(field.Label)).Class("mb-2"),
+			v.VItemGroup(
+				v.VItem(
+					v.VBtn("").Children(
+						Div(
+							Div(
+								Div().Style("width: 72px; height: 2px; background: #bdbdbd"),
+								Div().Style("width: 72px; height: 2px; background: #bdbdbd"),
+								Div().Style("width: 72px; height: 2px; background: #bdbdbd"),
+								Div().Style("width: 40px; height: 2px; background: #bdbdbd"),
+							).Class("d-flex justify-space-between flex-column").Style("height: 24px"),
+							Div().Class("ml-2").Style("background: #bdbdbd;width: 24px;height: 24px;border-radius: 2px;")).Class("d-flex justify-space-between"),
+					).
+						Class("mr-2 px-2").
+						Height(40).
+						Attr("@click", "toggle").
+						Attr(":class", "['d-flex', 'align-center']").
+						Attr(":color", "isSelected ? 'primary' : 'grey'").
+						Attr(":style", "{background: isSelected ? '#E6EDFE' : ''}").
+						Attr(":variant", "isSelected ? 'outlined' : 'tonal'"),
+				).
+					Value("left").
+					Attr("v-slot", "{ isSelected, toggle }"),
+				v.VItem(
+					v.VBtn("").Children(
+						Div(
+							Div().Class("mr-2").Style("background: #bdbdbd;width: 24px;height: 24px;border-radius: 2px;"),
+							Div(
+								Div().Style("width: 72px; height: 2px; background: #bdbdbd"),
+								Div().Style("width: 72px; height: 2px; background: #bdbdbd"),
+								Div().Style("width: 72px; height: 2px; background: #bdbdbd"),
+								Div().Style("width: 40px; height: 2px; background: #bdbdbd"),
+							).Class("d-flex justify-space-between flex-column").Style("height: 24px"),
+						).Class("d-flex justify-space-between"),
+					).
+						Class("mr-2 px-2").
+						Height(40).
+						Attr("@click", "toggle").
+						Attr(":class", "['d-flex', 'align-center']").
+						Attr(":color", "isSelected ? 'primary' : 'grey'").
+						Attr(":style", "{background: isSelected ? '#E6EDFE' : ''}").
+						Attr(":variant", "isSelected ? 'outlined' : 'tonal'"),
+				).Value("right").Attr("v-slot", "{ isSelected, toggle }"),
+			).Class("d-flex").
+				Attr(":mandatory", "true").
+				// "Style.Layout"
+				Attr(web.VField(field.FormKey, reflectutils.MustGet(obj, field.Name))...),
+		).Class("mb-4")
 	})
 
 	fb.Field("TopSpace").ComponentFunc(func(obj interface{}, field *presets.FieldContext, ctx *web.EventContext) HTMLComponent {
