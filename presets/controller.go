08fa6cf601fa4a9a3f158b342e456db8c4b22fc2
Ethan <wen@theplant.jp>
2025-01-08 09:47:15 +0800

    add tab controller

--- presets/controller.go
+++ presets/controller.go
@@ -3,6 +3,9 @@ package presets
 import (
 	"fmt"
 
+	"github.com/qor5/web/v3"
+	v "github.com/qor5/x/v3/ui/vuetify"
+	vx "github.com/qor5/x/v3/ui/vuetifyx"
 	h "github.com/theplant/htmlgo"
 )
 
@@ -28,4 +31,52 @@ func LinkageFieldsController(field *FieldContext, vs ...string) h.HTMLComponent 
 }
 }
 	}`, h.JSONString(vs)))
+}
+
+type (
+	TabsControllerOption struct {
+		DefaultIndex     int
+		Tabs             []TabControllerOption
+		WrapTabComponent func(*vx.VXTabsBuilder) *vx.VXTabsBuilder
+	}
+	TabControllerOption struct {
+		Tab    *v.VTabBuilder
+		Fields []string
+	}
+)
+
+func TabsController(field *FieldContext, option *TabsControllerOption) h.HTMLComponent {
+	if option == nil || len(option.Tabs) == 0 || option.DefaultIndex >= len(option.Tabs) {
+		return nil
+	}
+
+	var (
+		tabs      []h.HTMLComponent
+		tabFields [][]string
+	)
+	for index, tabController := range option.Tabs {
+		tabs = append(tabs, tabController.Tab.Value(index))
+		tabFields = append(tabFields, tabController.Fields)
+	}
+	vxTabs := vx.VXTabs(tabs...)
+	if option.WrapTabComponent != nil {
+		vxTabs = option.WrapTabComponent(vxTabs)
+	}
+
+	return web.Scope(
+		h.Div().Attr("v-on-mounted", fmt.Sprintf(`() => {
+tabsTabsControllerLocals.__setTabFieldVisible = (index) => {
+        dash.visible = {
+%v:true};
+        tabsTabsControllerLocals.items[index].forEach((fieldFormKey) => {
+            dash.visible[fieldFormKey] = true;
+        })}
+   tabsTabsControllerLocals.__setTabFieldVisible(tabsTabsControllerLocals.tab)
+}`, field.FormKey)).Style("display:none"),
+		vxTabs.Attr("@update:model-value",
+			`
+tabsTabsControllerLocals.__setTabFieldVisible($event)`).
+			Attr("v-model", "tabsTabsControllerLocals.tab"),
+	).VSlot("{locals:tabsTabsControllerLocals}").
+		Init(fmt.Sprintf("{tab:%v,items:%v}", option.DefaultIndex, h.JSONString(tabFields)))
 }
