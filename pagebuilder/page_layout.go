7130cf703262514319bb39fee7c4daadfc1f4234
Ethan <wen@theplant.jp>
2025-01-15 10:06:09 +0800

    WrapDefaultPageLayout

--- pagebuilder/page_layout.go
+++ pagebuilder/page_layout.go
@@ -1,6 +1,7 @@
 package pagebuilder
 
 import (
+	"embed"
 	"fmt"
 	"strings"
 
@@ -8,7 +9,25 @@ import (
 	h "github.com/theplant/htmlgo"
 )
 
+//go:embed assets/css/page-builder-theme.css
+var theme embed.FS
+
+func WrapDefaultPageLayoutFunc(css ...string) PageLayoutFunc {
+	return func(body h.HTMLComponent, input *PageLayoutInput, ctx *web.EventContext) h.HTMLComponent {
+		input.FreeStyleCss = append(input.FreeStyleCss, css...)
+		return pageLayoutFunc(body, input, ctx)
+	}
+}
+
 func defaultPageLayoutFunc(body h.HTMLComponent, input *PageLayoutInput, ctx *web.EventContext) h.HTMLComponent {
+	css, err := theme.ReadFile("assets/css/page-builder-theme.css")
+	if err != nil {
+		panic(err)
+	}
+	return WrapDefaultPageLayoutFunc(string(css))(body, input, ctx)
+}
+
+func pageLayoutFunc(body h.HTMLComponent, input *PageLayoutInput, ctx *web.EventContext) h.HTMLComponent {
 	var freeStyleCss h.HTMLComponent
 	if len(input.FreeStyleCss) > 0 {
 		freeStyleCss = h.Style(strings.Join(input.FreeStyleCss, "\n"))
@@ -22,134 +41,6 @@ func defaultPageLayoutFunc(body h.HTMLComponent, input *PageLayoutInput, ctx *we
 	tailwindJs := "https://cdn.tailwindcss.com"
 	alpineJs := "https://unpkg.com/alpinejs"
 	InferCss := "https://rsms.me/inter/inter.css"
-
-	pbThemeCss := []string{`
-@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Monoton&family=Pacifico&display=swap');
-
-:root {
-/**
-	--color-base: 47, 45, 39;
-	--color-surface: 255, 255, 255;
-	--color-brand: 137, 255, 176;
-	--font-family-h1: 'Monoton';
-	--font-family-h2: '';
-	--font-family-p: '';
-**/
-/**
-	--color-base: 255, 242, 185;
-	--color-surface: 153, 93, 15;
-	--color-brand: 229, 72, 77;
-	--font-family-h1: 'Pacifico';
-	--font-family-h2: 'Times New Roman';
-	--font-family-p: 'Times New Roman';
-**/
-
-	--color-base: 255, 255, 255;
-	--color-surface: 51, 51, 51;
-	--color-brand: 62, 99, 221;
-	--font-family-h1: '';
-	--font-family-h2: '';
-	--font-family-p: '';
-
-
-	--font-family-fallback: InterVariable, system-ui, sans-serif;
-
-	--pb-primary: #3e63dd;
-	--pb-secondary: #5b6471;
-	--pb-success: #30a46c;
-	--pb-info: #0091ff;
-	--pb-warning: #f76808;
-	--pb-error: #e5484d;
-}
-
-.tailwind-scope .tw-theme-h1 {
-	font-family: var(--font-family-h1), var(--font-family-fallback);
-}
-
-.tailwind-scope .tw-theme-h2 {
-	font-family: var(--font-family-h2), var(--font-family-fallback);
-}
-
-.tailwind-scope .tw-theme-p {
-	font-family: var(--font-family-p), var(--font-family-fallback);
-}
-
-.tw-theme-filter-container {
-	position: relative;
-}
-
-.tw-theme-filter-container::before {
-	--color-opacity: 0.2;
-	content: '';
-	position: absolute;
-	top: 0;
-	left: 0;
-	right: 0;
-	bottom: 0;
-	background-color: rgba(var(--color-brand), var(--color-opacity));
-	z-index: 1;
-	mix-blend-mode: multiply;
-	pointer-events: none;
-}
-
-.tw-theme-bg-base {
-	--color-opacity: 1;
-	background-color: rgba(var(--color-base), var(--color-opacity));
-}
-
-.tw-theme-text-base {
-	--color-opacity: 1;
-	color: rgba(var(--color-base), var(--color-opacity));
-}
-
-.tw-theme-bg-brand {
-	--color-opacity: 1;
-	background-color: var(
-		--tw-theme-bg-color,
-		rgba(var(--color-brand), var(--color-opacity))
-	);
-}
-
-.tw-theme-bg-brand-20 {
-	--color-opacity: 0.2;
-	background-color: var(
-		--tw-theme-bg-color,
-		rgba(var(--color-brand), var(--color-opacity))
-	);
-}
-
-.tw-theme-text {
-	--color-opacity: 1;
-	color: rgba(var(--color-surface), var(--color-opacity));
-}
-
-.btn-primary {
-  color: #fff;
-  background-color: var(--pb-primary);
-}
-.btn-secondary {
-  color: #fff;
-  background-color: var(--pb-secondary);
-}
-.btn-info {
-  color: #fff;
-  background-color: var(--pb-info);
-}
-.btn-success {
-  color: #fff;
-  background-color: var(--pb-success);
-}
-.btn-error {
-  color: #fff;
-  background-color: var(--pb-error);
-}
-.btn-warning {
-  color: #fff;
-  background-color: var(--pb-warning);
-}
-
-	`}
 
 	tailwindPluginJs := []string{
 		`tailwind.config = {
@@ -199,7 +90,6 @@ func defaultPageLayoutFunc(body h.HTMLComponent, input *PageLayoutInput, ctx *we
 		input.StructuredData,
 		scriptWithCodes(input.FreeStyleTopJs),
 		scriptWithCodes(tailwindPluginJs),
-		styleWithCodes(pbThemeCss),
 		styleWithCodes(tailwindOverrides),
 	)
 	ctx.Injector.HTMLLang(input.LocaleCode)
