623075fa04a6fc97315b94bed65e0a619966cf76
Ethan <wen@theplant.jp>
2025-03-05 10:04:09 +0800

    email builder use custom page

--- emailbuilder/model_builder.go
+++ emailbuilder/model_builder.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/mail"
+	"path"
 	"strconv"
 	"strings"
 
@@ -18,7 +19,6 @@ import (
 	v "github.com/qor5/x/v3/ui/vuetify"
 	vx "github.com/qor5/x/v3/ui/vuetifyx"
 	"github.com/samber/lo"
-	"github.com/sunfmin/reflectutils"
 	h "github.com/theplant/htmlgo"
 	"gorm.io/gorm"
 
@@ -39,7 +39,7 @@ const (
 	EmailEditorDialogPortalName = "emailEditorDialog"
 )
 
-func (mb *ModelBuilder) Install(_ *presets.Builder) (err error) {
+func (mb *ModelBuilder) Install(b *presets.Builder) (err error) {
 	if mb.b.ab != nil {
 		mb.mb.Use(mb.b.ab)
 	}
@@ -68,61 +68,17 @@ func (mb *ModelBuilder) Install(_ *presets.Builder) (err error) {
 			return in(obj, id, ctx)
 		}
 	})
+	b.HandleCustomPage(mb.editorPattern(), presets.NewCustomPage(b).Body(mb.emailBuilderBody).Menu(func(*web.EventContext) h.HTMLComponent {
+		return nil
+	}))
 	dp := mb.mb.Detailing()
 	if mb.IsTpl {
-		dp.Title(func(evCtx *web.EventContext, obj any, style presets.DetailingStyle, defaultTitle string) (title string, titleCompo h.HTMLComponent, err error) {
-			titleCompo = h.Div(
-				v.VToolbarTitle("Inbox"),
-				v.VSpacer(),
-				vx.VXBtn("Save").Variant("elevated").Attr("@click", web.Emit("save_mail")).Color("primary").Attr(":disabled", "vars.$EmailEditorLoading"),
-				vx.VXBtn("Send Email").
-					Variant("elevated").
-					Color("secondary").
-					Attr("@click", web.Emit("open_send_mail_dialog")).
-					Color("secondary").
-					Class("ml-2"),
-			).Class("d-flex align-center w-100")
-
-			return
+		mb.mb.Listing().WrapCell(func(in presets.CellProcessor) presets.CellProcessor {
+			return func(evCtx *web.EventContext, cell h.MutableAttrHTMLComponent, id string, obj any) (h.MutableAttrHTMLComponent, error) {
+				cell.SetAttr("@click", web.Plaid().URL(mb.editorUri(id)).PushState(true).Go())
+				return cell, nil
+			}
 		})
-		dp.GetField(EmailEditorField).
-			ComponentFunc(func(obj interface{}, field *presets.FieldContext, ctx *web.EventContext) h.HTMLComponent {
-				var (
-					primarySlug = mb.primarySlug(obj)
-				)
-
-				return h.Div(
-					web.Listen("save_mail",
-						fmt.Sprintf(`() => { $refs.emailEditor.emit('getData').then(res=> {%s})}`,
-							web.Plaid().EventFunc(actions.Update).Query(presets.ParamID, primarySlug).Form(web.Var("res")).Go())),
-					web.Listen("open_send_mail_dialog", ShowDialogScript(EmailEditorDialogPortalName, UtilDialogPayloadType{
-						Title: "Please Enter a Email Address",
-						ContentEl: vx.VXField().
-							Label("To").
-							Placeholder("Enter a email address").
-							Attr("v-model", "vars.to_email_address").
-							// Attr(":rules", "(v) => !!v || 'Email is required'").
-							Required(true),
-						OkAction:  web.Emit("send_mail"),
-						LoadingOk: "vars.$EmailEditorLoading",
-					})),
-
-					web.Listen("send_mail", fmt.Sprintf(`(payload)=> {
-						vars.$EmailEditorLoading = true
-
-						$refs.emailEditor.emit('sendMail', {to: vars.to_email_address})
-							.then(res=> { %s })
-							.catch(err=> { %s }) 
-							.finally(()=> { vars.$EmailEditorLoading = false })
-					}`, presets.ShowSnackbarScript("Email successfully sent.", "success"), presets.ShowSnackbarScript("Email sent failed.", "error"))),
-					vx.VXIframeEmailEditor().
-						Ref("emailEditor").
-						Src(fmt.Sprintf("/email_builder/editor?id=%s&userId=undefined", primarySlug)).
-						Class("flex-1 ml-2"),
-
-					web.Portal().Name(EmailEditorDialogPortalName),
-				).Class("d-flex").Style("height: calc(100vh - 100px - 20px);")
-			})
 	} else {
 		dp.GetField(EmailDetailField).ComponentFunc(mb.mailDetailFieldCompoFunc())
 		if mb.b.ab != nil {
@@ -404,14 +360,6 @@ func (mb *ModelBuilder) setEmailDefaultValue(et *EmailDetail) {
 }`, strconv.Quote(et.Subject))
 }
 
-func (mb *ModelBuilder) primarySlug(obj interface{}) (s string) {
-	p, ok := obj.(presets.SlugEncoder)
-	if ok {
-		return p.PrimarySlug()
-	}
-	return fmt.Sprintf("%v", reflectutils.MustGet(obj, "ID"))
-}
-
 func (mb *ModelBuilder) mailDetailFieldCompoFunc() presets.FieldComponentFunc {
 
 	return func(obj interface{}, field *presets.FieldContext, ctx *web.EventContext) h.HTMLComponent {
@@ -445,8 +393,90 @@ transform-origin: 0 0; transform:scale(0.5);width:200%;height:200%`),
 				).Class("pa-6 w-100 d-flex justify-space-between align-center").Style(`position:absolute;bottom:0;left:0`),
 			).Style(`position:relative;height:320px;width:100%`).Class("border-thin rounded-lg").
 				Attr("@click",
-					web.Plaid().URL(mb.b.getTemplateModelBuilder().mb.Info().DetailingHref("1")).Go(),
+					web.Plaid().URL(mb.b.getTemplateModelBuilder().editorUri("1")).Go(),
 				),
 		).Class("my-10")
 	}
+}
+
+func (mb *ModelBuilder) editorPattern() string {
+	return path.Join(mb.mb.Info().URIName(), "/email_builder/editor")
+}
+
+func (mb *ModelBuilder) editorUri(primarySlug string) string {
+	return fmt.Sprintf("/%s?%s=%s", path.Join(mb.mb.GetPresetsBuilder().GetURIPrefix(), mb.editorPattern()), presets.ParamID, primarySlug)
+}
+
+func (mb *ModelBuilder) emailBuilderBody(ctx *web.EventContext) h.HTMLComponent {
+	var (
+		primarySlug = ctx.Param(presets.ParamID)
+		exitHref    string
+	)
+	if mb.IsTpl {
+		exitHref = mb.mb.Info().ListingHref()
+	} else {
+		exitHref = mb.mb.Info().DetailingHref(primarySlug)
+	}
+
+	return v.VContainer().Children(
+
+		h.Div(
+			h.Div(
+				h.Div().Style("transform:rotateY(180deg)").Class("mr-4").Children(
+					v.VIcon("mdi-exit-to-app").Attr("@click", fmt.Sprintf(`
+						const last = vars.__history.last();
+						if (last && last.url && last.url === %q) {
+							$event.view.window.history.back();
+							return;
+						}
+						%s`, exitHref, web.GET().URL(exitHref).PushState(true).Go(),
+					)),
+				),
+				v.VToolbarTitle("Inbox"),
+				v.VSpacer(),
+				vx.VXBtn("Save").Variant("elevated").Attr("@click", web.Emit("save_mail")).Color("primary").Attr(":disabled", "vars.$EmailEditorLoading"),
+				vx.VXBtn("Send Email").
+					Variant("elevated").
+					Color("secondary").
+					Attr("@click", web.Emit("open_send_mail_dialog")).
+					Color("secondary").
+					Class("ml-2"),
+			).Class("d-flex align-center w-100"),
+		).Class("d-flex align-center pa-3  w-100"),
+
+		h.Div().Class("d-flex flex-column").Children(
+			h.Div(
+				web.Listen("save_mail",
+					fmt.Sprintf(`() => { $refs.emailEditor.emit('getData').then(res=> {%s})}`,
+						web.Plaid().EventFunc(actions.Update).Query(presets.ParamID, primarySlug).Form(web.Var("res")).Go())),
+				web.Listen("open_send_mail_dialog", ShowDialogScript(EmailEditorDialogPortalName, UtilDialogPayloadType{
+					Title: "Please Enter a Email Address",
+					ContentEl: vx.VXField().
+						Label("To").
+						Placeholder("Enter a email address").
+						Attr("v-model", "vars.to_email_address").
+						// Attr(":rules", "(v) => !!v || 'Email is required'").
+						Required(true),
+					OkAction:  web.Emit("send_mail"),
+					LoadingOk: "vars.$EmailEditorLoading",
+				})),
+
+				web.Listen("send_mail", fmt.Sprintf(`(payload)=> {
+						vars.$EmailEditorLoading = true
+
+						$refs.emailEditor.emit('sendMail', {to: vars.to_email_address})
+							.then(res=> { %s })
+							.catch(err=> { %s }) 
+							.finally(()=> { vars.$EmailEditorLoading = false })
+					}`, presets.ShowSnackbarScript("Email successfully sent.", "success"), presets.ShowSnackbarScript("Email sent failed.", "error"))),
+				vx.VXIframeEmailEditor().
+					Ref("emailEditor").
+					Src(fmt.Sprintf("/email_builder/editor?id=%s&userId=undefined", primarySlug)).
+					Class("flex-1 ml-2"),
+
+				web.Portal().Name(EmailEditorDialogPortalName),
+			).Class("d-flex").Style("height: calc(100vh - 100px - 20px);"),
+		),
+	).Fluid(true).Class("px-0 detailing-page-wrap")
+
 }
