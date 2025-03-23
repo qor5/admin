4223f549bf293dd33b621945005e6ff045ca8756
Daniel <contact@danni.cool>
2025-03-23 21:38:43 +0800

    refactor: update common-container styles to use new CSS variables and remove deprecated theme-model file for improved consistency and maintainability

--- pagebuilder/commonContainer/imageWithText/index.go
+++ pagebuilder/commonContainer/imageWithText/index.go
@@ -119,62 +119,60 @@ func ImageWithTextBody(data *ImageWithText, input *pagebuilder.RenderInput) (bod
 
 	hasHeroImage := lo.Contains(data.Style.Visibility, "image")
 
-	heroBody := Div(
-		Div(Div(
+	heroBody := Div(Div(
+		Div(
 			Div(
 				Div(
-					Div(
-						If(lo.Contains(data.Style.Visibility, "title"), H1("").Children(RawHTML(data.Content.Title)).
-							Class("richEditor-content tw-theme-text tw-theme-h1 font-medium xl:text-[80px] md:text-[48px] text-[25.875px] xl:leading-[98px] md:leading-normal leading-[31.697px]")),
-						If(lo.Contains(data.Style.Visibility, "content"), Div().Children(RawHTML(data.Content.Content)).Class("richEditor-content tw-theme-text tw-theme-p xl:text-[24px] md:text-[22px] text-[12px] font-medium xl:leading-[32px] leading-normal").
-							ClassIf("text-left", data.Style.HorizontalAlign == "item-start").
-							ClassIf("text-center", data.Style.HorizontalAlign == "items-center").
-							ClassIf("text-right", data.Style.HorizontalAlign == "items-end")),
-						If(lo.Contains(data.Style.Visibility, "button"), Div(
-							A().Attr("href", data.Content.ButtonHref).Attr("target", "_blank").Attr("rel", "noopener noreferrer").Children(Button(data.Content.Button).
-								Class("tw-theme-bg-brand tw-theme-text-base tw-theme-p xl:text-[16px] md:text-[14px] text-[12px] xl:px-6 xl:py-3 md:px-4 md:py-2 px-3 py-[6px] rounded-[4px]")),
-						)),
-					).Attr("x-ref", "leftContent").Class(fmt.Sprintf("flex flex-col xl:gap-10 md:gap-6 gap-3 h-full %s %s", data.Style.VerticalAlign, data.Style.HorizontalAlign)),
-				).ClassIf("order-2 xl:ml-10 md:ml-[20px] ml-6", data.Style.Layout == "right"),
-				If(hasHeroImage, Div(
-					Div(
-						Img(heroImgUrl).Class("position-absolute w-full object-cover object-center h-full max-w-full left-0 top-0 flex-shrink-0"),
-					).
-						Class(fmt.Sprintf("tw-theme-filter-container flex-shrink-0 overflow-hidden xl:h-[%s] xl:w-[%s] md:h-[%s] md:w-[%s] h-[%s] w-[%s]",
-							imageHeight,
-							imageWidth,
-							getScaledImageDimension(imageHeight, 314.0/500.0),
-							getScaledImageDimension(imageWidth, 314.0/500.0),
-							getScaledImageDimension(imageHeight, 169.0/500.0),
-							getScaledImageDimension(imageWidth, 169.0/500.0),
-						)),
-				).Class(fmt.Sprintf("order-1 flex flex-col items-center %s", data.Style.VerticalAlign)).
-					ClassIf("xl:ml-10 md:ml-[20px] ml-6", data.Style.Layout == "left").
-					ClassIf("justify-center", data.Style.VerticalAlign == "justify-between")),
-			).Class(fmt.Sprintf("flex justify-between xl:max-w-[1280px] md:max-w-[768px] max-w-[414px] mx-auto xl:pl-[%dpx] xl:pr-[%dpx]  md:pl-[%dpx] md:pr-[%dpx] pl-[%dpx] pr-[%dpx] xl:pt-[%dpx] xl:pb-[%dpx] md:pt-[%dpx] md:pb-[%dpx]  pt-[%dpx] pb-[%dpx]",
-				data.Style.LeftSpace,
-				data.Style.RightSpace,
-				int(float64(data.Style.LeftSpace)*0.5),
-				int(float64(data.Style.RightSpace)*0.5),
-				int(float64(data.Style.LeftSpace)*0.26),
-				int(float64(data.Style.RightSpace)*0.26),
-				data.Style.TopSpace,
-				data.Style.BottomSpace,
-				int(float64(data.Style.TopSpace)*0.5),
-				int(float64(data.Style.BottomSpace)*0.5),
-				int(float64(data.Style.TopSpace)*0.26),
-				int(float64(data.Style.BottomSpace)*0.26),
-			)).Attr("x-data", fmt.Sprintf(`{
+					If(lo.Contains(data.Style.Visibility, "title"), H1("").Children(RawHTML(data.Content.Title)).
+						Class("richEditor-content tw-theme-text tw-theme-h1 font-medium xl:text-[80px] md:text-[48px] text-[25.875px] xl:leading-[98px] md:leading-normal leading-[31.697px]")),
+					If(lo.Contains(data.Style.Visibility, "content"), Div().Children(RawHTML(data.Content.Content)).Class("richEditor-content tw-theme-text tw-theme-p xl:text-[24px] md:text-[22px] text-[12px] font-medium xl:leading-[32px] leading-normal").
+						ClassIf("text-left", data.Style.HorizontalAlign == "item-start").
+						ClassIf("text-center", data.Style.HorizontalAlign == "items-center").
+						ClassIf("text-right", data.Style.HorizontalAlign == "items-end")),
+					If(lo.Contains(data.Style.Visibility, "button"), Div(
+						A().Attr("href", data.Content.ButtonHref).Attr("target", "_blank").Attr("rel", "noopener noreferrer").Children(Button(data.Content.Button).
+							Class("tw-theme-bg-brand tw-theme-text-base tw-theme-p xl:text-[16px] md:text-[14px] text-[12px] xl:px-6 xl:py-3 md:px-4 md:py-2 px-3 py-[6px] rounded-[4px]")),
+					)),
+				).Attr("x-ref", "leftContent").Class(fmt.Sprintf("flex flex-col xl:gap-10 md:gap-6 gap-3 h-full %s %s", data.Style.VerticalAlign, data.Style.HorizontalAlign)),
+			).ClassIf("order-2 xl:ml-10 md:ml-[20px] ml-6", data.Style.Layout == "right"),
+			If(hasHeroImage, Div(
+				Div(
+					Img(heroImgUrl).Class("position-absolute w-full object-cover object-center h-full max-w-full left-0 top-0 flex-shrink-0"),
+				).
+					Class(fmt.Sprintf("tw-theme-filter-container flex-shrink-0 overflow-hidden xl:h-[%s] xl:w-[%s] md:h-[%s] md:w-[%s] h-[%s] w-[%s]",
+						imageHeight,
+						imageWidth,
+						getScaledImageDimension(imageHeight, 314.0/500.0),
+						getScaledImageDimension(imageWidth, 314.0/500.0),
+						getScaledImageDimension(imageHeight, 169.0/500.0),
+						getScaledImageDimension(imageWidth, 169.0/500.0),
+					)),
+			).Class(fmt.Sprintf("order-1 flex flex-col items-center %s", data.Style.VerticalAlign)).
+				ClassIf("xl:ml-10 md:ml-[20px] ml-6", data.Style.Layout == "left").
+				ClassIf("justify-center", data.Style.VerticalAlign == "justify-between")),
+		).Class(fmt.Sprintf("flex justify-between xl:max-w-[1280px] md:max-w-[768px] max-w-[414px] mx-auto xl:pl-[%dpx] xl:pr-[%dpx]  md:pl-[%dpx] md:pr-[%dpx] pl-[%dpx] pr-[%dpx] xl:pt-[%dpx] xl:pb-[%dpx] md:pt-[%dpx] md:pb-[%dpx]  pt-[%dpx] pb-[%dpx]",
+			data.Style.LeftSpace,
+			data.Style.RightSpace,
+			int(float64(data.Style.LeftSpace)*0.5),
+			int(float64(data.Style.RightSpace)*0.5),
+			int(float64(data.Style.LeftSpace)*0.26),
+			int(float64(data.Style.RightSpace)*0.26),
+			data.Style.TopSpace,
+			data.Style.BottomSpace,
+			int(float64(data.Style.TopSpace)*0.5),
+			int(float64(data.Style.BottomSpace)*0.5),
+			int(float64(data.Style.TopSpace)*0.26),
+			int(float64(data.Style.BottomSpace)*0.26),
+		)).Attr("x-data", fmt.Sprintf(`{
 			hasHeroImage: %t,
 		}`, hasHeroImage)),
-		).Class("tw-theme-bg-brand-20 text-[#212121] bg-no-repeat bg-cover bg-center"),
-		// Style(fmt.Sprintf("background-image: url(%s)", backgroundImgUrl)),
-		).Class(" tw-theme-bg-base"),
-	).Class("container-hero-inner")
+	).Class("text-[#212121] bg-no-repeat bg-cover bg-center"),
+	// Style(fmt.Sprintf("background-image: url(%s)", backgroundImgUrl)),
+	).Class("tw-theme-bg-base common-container-inner bg-no-repeat bg-cover bg-center")
 
 	body = utils.TailwindContainerWrapper(
-		"container-hero",
-		Tag("twind-scope").Attr("data-props", fmt.Sprintf(`{"type":"imageWithText", "id": %q}`, input.ContainerId)).Children(Div(heroBody).Class("bg-gray-100")),
+		"common-container",
+		Tag("twind-scope").Attr("data-props", fmt.Sprintf(`{"type":"container-image-with-text", "id": %q}`, input.ContainerId)).Children(heroBody),
 	)
 	return
 }
