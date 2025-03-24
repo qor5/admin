0eea0b8e6d0da0b3bdea71c955b62920cb74b10c
Ethan <wen@theplant.jp>
2025-03-24 15:02:58 +0800

    add arg job test

--- example/integration/worker_test.go
+++ example/integration/worker_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/qor5/web/v3/multipartestutils"
 
 	"github.com/qor5/admin/v3/example/admin"
+	"github.com/qor5/admin/v3/presets/actions"
 )
 
 func TestWorker(t *testing.T) {
@@ -27,6 +28,35 @@ func TestWorker(t *testing.T) {
 				return req
 			},
 			ExpectPortalUpdate0ContainsInOrder: []string{`F1`},
+		},
+		{
+			Name:  "Worker Arg Job ",
+			Debug: true,
+			ReqFunc: func() *http.Request {
+				admin.PostsExampleData.TruncatePut(dbr)
+				req := multipartestutils.NewMultipartBuilder().
+					PageURL("/workers").
+					EventFunc("worker_selectJob").
+					Query("jobName", "argJob").
+					BuildEventFuncRequest()
+				return req
+			},
+			ExpectPortalUpdate0ContainsInOrder: []string{`F1`, "F2", "F3"},
+		},
+		{
+			Name:  "Worker Arg Job Validate",
+			Debug: true,
+			ReqFunc: func() *http.Request {
+				admin.PostsExampleData.TruncatePut(dbr)
+				req := multipartestutils.NewMultipartBuilder().
+					PageURL("/workers").
+					EventFunc(actions.Update).
+					AddField("Job", "argJob").
+					AddField("F1", "aaa").
+					BuildEventFuncRequest()
+				return req
+			},
+			ExpectPortalUpdate0ContainsInOrder: []string{`cannot be aaa`},
 		},
 	}
 
