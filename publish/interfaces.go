a596c166524d25f4e2627ede4c8ad1a00167d2c5
Ethan <wen@theplant.jp>
2025-04-24 16:30:55 +0800

    wip

--- publish/interfaces.go
+++ publish/interfaces.go
@@ -141,7 +141,7 @@ func (s *List) EmbedList() *List {
 
 type (
 	PreviewBuilderInterface interface {
-		PreviewHTML(obj interface{}, ctx context.Context) string
+		PreviewHTML(ctx context.Context, obj interface{}) string
 		ExistedL10n() bool
 	}
 	PublishModelInterface interface {
